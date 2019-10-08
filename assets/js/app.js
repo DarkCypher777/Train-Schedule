123d78dba64705625ce47a5cdf9a180d12fed63d
Ben <flamedarkness666@gmail.com>
2019-10-08 13:11:47 -0400

    initial site files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,109 @@
-$(document).ready(function () {
+// 
 
-    // Initialize Firebase
-    // This is the code we copied and pasted from our app page
-    var config = {
-        apiKey: "AIzaSyAjv_Myuo-Wu4t-KIxKae0f2ltJ8toy81U",
-        authDomain: "bootcamp-train-schedule.firebaseapp.com",
-        databaseURL: "https://bootcamp-train-schedule.firebaseio.com",
-        projectId: "bootcamp-train-schedule",
-        storageBucket: "",
-        messagingSenderId: "1094548381366",
-        appId: "1:1094548381366:web:ee9346eac6df4ab7d99017",
-        measurementId: "G-DG03NW9498"
+// * Firebase config
+var config = {
+    apiKey: "AIzaSyAjv_Myuo-Wu4t-KIxKae0f2ltJ8toy81U",
+    authDomain: "bootcamp-train-schedule.firebaseapp.com",
+    databaseURL: "https://bootcamp-train-schedule.firebaseio.com",
+    projectId: "bootcamp-train-schedule",
+    storageBucket: "",
+    messagingSenderId: "1094548381366",
+    appId: "1:1094548381366:web:ee9346eac6df4ab7d99017",
+    measurementId: "G-DG03NW9498"
+};
+
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+// Initial Variables (SET the first set IN FIREBASE FIRST)
+// Note remember to create these same variables in Firebase!
+$("#confirm-train").on("click", function (event) {
+    // Prevent the page from refreshing
+    event.preventDefault();
+
+    // user input
+    // TODO use https://momentjs.com/docs/#/parsing/string-format/ for time
+    var trainName = $("#name-input").val().trim();
+    var trainDest = $("#dest-input").val().trim();
+    var trainTime = $("#time-input").val().trim();
+    var trainFreq = $("#freq-input").val().trim();
+
+    // Creates local temp object for holding train data
+    var newTrain = {
+        name: trainName,
+        destination: trainDest,
+        time: trainTime,
+        frequency: trainFreq
     };
 
-    firebase.initializeApp(config);
+    // Uploads train data to the database
+    database.ref().push(newTrain);
 
-    // Get a reference to the database service
-    var database = firebase.database();
+    // console logs
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.time);
+    console.log(newTrain.frequency);
 
-    // At the initial load and on subsequent data value changes, get a snapshot of the current data. (I.E FIREBASE HERE)
-    // This callback keeps the page updated when a value changes in firebase. ****Firebase Code HERE****
-    database.ref().on("value", function (snapshot) {
-        console.log(snapshot.val());
+    alert("Train Added")
+
+    // clear text box after input
+    $("#name-input") > val("");
+    $("#dest-input") > val("");
+    $("#time-input") > val("");
+    $("#freq-input") > val("");
+});;
+
+//  At the initial load and on subsequent data value changes, get a snapshot of the current data. (I.E FIREBASE HERE)
+// This callback keeps the page updated when a value changes in firebase. ****Firebase Code HERE****
+// Firebase is always watching for changes to the data.
+// When changes occurs it will print them to console and html
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(snapshot.val());
+
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination
+    var trainTime = childSnapshot.val().time
+    var trainFreq = childSnapshot.val().frequency
+
+    // console log
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(trainTime);
+    console.log(trainFreq);
+
+    // make it look neat and clean
+    var trainTimeUnify = moment.unix(trainTime).format("MM/DD/YYYY");
+
+    // calculate next arivial
+    // var empMonths = moment().diff(moment(trainTime, "X"), "months");
+    // console.log(empMonths);
+
+    // calculate minutes away
 
 
-        // If any errors are experienced, log them to console.
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
+    // create new row
+    var newRow = $("<>").append(
+        $("<>").text(trainName),
+        $("<>").text(trainDest),
+        $("<>").text(trainFreq),
+        $("<>").text(trainTime),
+        $("<>").text(trainProx)
+    )
 
+      // Append the new row to the table
+  $("#employee-table > tbody").append(newRow);
 
-    // Whenever a user clicks the confirm-train button
-    $("#confirm-train").on("click", function () {
-
-        
-        // Save new value to Firebase
-        database.ref().set({
-            // clickCount: clickCounter
-        });
-
-        // Log the value of 
-        console.log();
-
-    });
-
-    // database.ref().set({
-    //     clickCount: clickCounter
-    //   });
+    // If any errors are experienced, log them to console.
+}, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
 });
+
+
+
+
+// database.ref().set({
+//     clickCount: clickCounter
+//   });
