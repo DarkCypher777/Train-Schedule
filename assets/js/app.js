2ba4f84b04543c651a6d8b642d5e9dade4045723
Ben <flamedarkness666@gmail.com>
2019-10-08 09:55:34 -0400

    initial site files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,50 @@
+$(document).ready(function () {
 
-// Initialize Firebase
-// This is the code we copied and pasted from our app page
-var config = {
-    apiKey: "AIzaSyAjv_Myuo-Wu4t-KIxKae0f2ltJ8toy81U",
-    authDomain: "bootcamp-train-schedule.firebaseapp.com",
-    databaseURL: "https://bootcamp-train-schedule.firebaseio.com",
-    projectId: "bootcamp-train-schedule",
-    storageBucket: "",
-    messagingSenderId: "1094548381366",
-    appId: "1:1094548381366:web:ee9346eac6df4ab7d99017",
-    measurementId: "G-DG03NW9498"
-};
+    // Initialize Firebase
+    // This is the code we copied and pasted from our app page
+    var config = {
+        apiKey: "AIzaSyAjv_Myuo-Wu4t-KIxKae0f2ltJ8toy81U",
+        authDomain: "bootcamp-train-schedule.firebaseapp.com",
+        databaseURL: "https://bootcamp-train-schedule.firebaseio.com",
+        projectId: "bootcamp-train-schedule",
+        storageBucket: "",
+        messagingSenderId: "1094548381366",
+        appId: "1:1094548381366:web:ee9346eac6df4ab7d99017",
+        measurementId: "G-DG03NW9498"
+    };
 
-firebase.initializeApp(config);
+    firebase.initializeApp(config);
 
-// Get a reference to the database service
-var database = firebase.database();
+    // Get a reference to the database service
+    var database = firebase.database();
+
+    // At the initial load and on subsequent data value changes, get a snapshot of the current data. (I.E FIREBASE HERE)
+    // This callback keeps the page updated when a value changes in firebase. ****Firebase Code HERE****
+    database.ref().on("value", function (snapshot) {
+        console.log(snapshot.val());
+
+
+        // If any errors are experienced, log them to console.
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
+
+    // Whenever a user clicks the confirm-train button
+    $("#confirm-train").on("click", function () {
+
+        
+        // Save new value to Firebase
+        database.ref().set({
+            // clickCount: clickCounter
+        });
+
+        // Log the value of 
+        console.log();
+
+    });
+
+    // database.ref().set({
+    //     clickCount: clickCounter
+    //   });
+});
